aaf99abf229e91ddbc663d476fc9c5a42c48f3ec
theo <theo.godard16@gmail.com>
2021-01-03 19:46:14 +0100

    hitbox lac ok

---
+++ build/grahalib/CMakeFiles/grahalib.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/theo/Bureau/Projet/Graha/grahalib/src/Game.cpp" "/home/theo/Bureau/Projet/Graha/build/grahalib/CMakeFiles/grahalib.dir/src/Game.cpp.o"
+  "/home/theo/Bureau/Projet/Graha/grahalib/src/Planet.cpp" "/home/theo/Bureau/Projet/Graha/build/grahalib/CMakeFiles/grahalib.dir/src/Planet.cpp.o"
+  "/home/theo/Bureau/Projet/Graha/grahalib/src/Text.cpp" "/home/theo/Bureau/Projet/Graha/build/grahalib/CMakeFiles/grahalib.dir/src/Text.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL"
+  "../glimac/include"
+  "../modellib/include"
+  "../grahalib/include"
+  "../third-party/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/modellib/CMakeFiles/modellib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modellib.dir/src/Cylinder.cpp.o"
+  "CMakeFiles/modellib.dir/src/Mesh.cpp.o"
+  "CMakeFiles/modellib.dir/src/Model.cpp.o"
+  "CMakeFiles/modellib.dir/src/Rectangle.cpp.o"
+  "libmodellib.pdb"
+  "libmodellib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/modellib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
